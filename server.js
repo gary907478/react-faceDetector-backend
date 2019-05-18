596e8678391ee19dc94a278afc8b8e0eedbda881
gary907478 <0619cloudz@gmail.com>
2019-05-18 17:10:11 -0400

    update database to heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-fluffy-23431',
     user: 'cloudz404',
     password: '',
     database: 'smart-brain'
